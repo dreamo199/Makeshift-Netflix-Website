be2260b8641b93dcc46a12ad17acac8cf9b7d48e
dreamo199 <alagbaosokelvin@gmail.com>
2025-12-22 16:10:32 +0100

    Another one

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";   
-import Logo from "./logo";
+import Logo from "./Logo";
 
 const AuthLayout = ({ children }) => (
   
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,6 @@
 const Logo = () => (
   <div className="text-2xl font-extrabold tracking-tight text-white select-none">
-    <span className="text-red-500 mr-2">DREAMY</span>BULL
+    <span className="text-blue-400 mr-2">DREAMY</span>BULL
   </div>
 );
 
